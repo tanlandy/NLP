2613d611df1268808f897ca2815752fb015fc778
Kunlin <kunlin.tan@iston.ai>
2023-08-25 19:56:39 +0800

    update

---
+++ PyTorch/13_feed_forward_nn.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
+# device config
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# hyper parameters
+input_size = 784  # 28x28
+hidden_size = 100
+num_classes = 10
+num_epochs = 2
+batch_size = 100
+learning_rate = 0.001
+
+# MNIST
+train_dataset = torchvision.datasets.MNIST(root='./data', train=True, transform=transforms.ToTensor(), download=True)
+test_dataset = torchvision.datasets.MNIST(root='./data', train=False, transform=transforms.ToTensor(), download=False)
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+examples = iter(train_loader)
+samples, labels = examples.next()
+print(samples.shape, labels.shape)
+
+# for i in range(6):
+#     plt.subplot(2, 3, i+1)
+#     plt.imshow(samples[i][0], cmap='gray')
